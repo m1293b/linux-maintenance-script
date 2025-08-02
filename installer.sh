33a1faa09b7bed29f670eb82a5e07a1ea9994892
m1293b <161032789+m1293b@users.noreply.github.com>
2025-08-02 21:36:29 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -10,7 +10,7 @@ echo "--- Preparing Maintenance Script ---"
 
 # Define the URL for the main script on GitHub
 # IMPORTANT: Change this URL to point to YOUR main script's raw file on GitHub
-MAIN_SCRIPT_URL="https://raw.githubusercontent.com/YourUsername/YourRepoName/main/main_script.sh"
+MAIN_SCRIPT_URL="https://raw.githubusercontent.com/m1293b/linux-maintenance-script/refs/heads/main/linux-maintenance.sh"
 
 # Define where to save the script temporarily
 TEMP_SCRIPT_PATH="/tmp/main_maintenance_script.sh"
